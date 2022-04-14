3223713e7743c02e3b95bd4149d0ea184a4183cb
Michael Scholz <m@scholz.moe>
2022-04-14 11:52:10 +0100

    Add minimal raytracing project
    Following https://raytracing.github.io/books/RayTracingInOneWeekend.html

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+
+    const int image_width = 256;
+    const int image_height = 256;
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int y = image_height - 1; y >= 0; --y) {
+        for (int x = 0; x < image_width; ++x) {
+            auto r = double(x) / (image_width - 1);
+            auto g = double(y) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.99 * r);
+            int ig = static_cast<int>(255.99 * g);
+            int ib = static_cast<int>(255.99 * b);
+
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    return 0;
+}
