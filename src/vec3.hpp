2877ed4d01c8afc355f6fb4b771451218d3796c4
Michael Scholz <m@scholz.moe>
2022-04-14 12:45:14 +0100

    Add the vec3 utility functions

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -60,4 +60,55 @@ class vec3 {
 using point3 = vec3;
 using color = vec3;
 
+// Print vec3 to output stream
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << " " << v.e[1] << " " << v.e[2];
+}
+
+// Element-wise add two vec3
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+// And element-wise subtract
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+// Element-wise multiply
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+// Multiply by a scalar (s * v and v * s)
+inline vec3 operator*(double t, const vec3 &v) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+// Divide by a scalar
+inline vec3 operator/(const vec3 &v, double t) {
+    return (1 / t) * v;
+}
+
+// Dot product aka scalar product
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+// Cross product to get the normal vector
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+// Normalise vector to unit
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
 #endif
