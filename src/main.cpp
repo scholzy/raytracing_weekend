09f182ba08452400274dbe335c7fb81fc7718436
Michael Scholz <m@scholz.moe>
2022-04-15 23:08:17 +0100

    Make abstraction for hittable objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,20 @@
 #include <iostream>
 
+#include "rtweekend.hpp"
+
 #include "color.hpp"
-#include "ray.hpp"
-#include "vec3.hpp"
+#include "hittable_list.hpp"
+#include "sphere.hpp"
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    // We're looking to solve (P(t) - C)(P(t) - C) = r^2
-    // This expands to:
-    //   b * b * t^2 + 2 * (A - C) * b  * t + (A - C) * (A - C) * r^2 = 0
-    //
-    // We can then use the determinant of the quadratric to check if the
-    // ray hits the sphere.
-    //
-    // oc = (A - C)
-    vec3 oc = r.origin() - center;
-    // a = b * b
-    auto a = r.direction().length_squared();
-    // b = 2 * b * (A - C) = 2 * b * oc
-    auto half_b = dot(oc, r.direction());
-    // c = (A - C) * (A - C) - r^2 = oc * oc - r^2
-    auto c = oc.length_squared() - radius * radius;
-    // \Delta = b^2 - 4 * a * c
-    auto discriminant = half_b * half_b - a * c;
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant)) / a;
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    // If the ray hits the sphere:
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
+    
     vec3 unit_direction = unit_vector(r.direction());
-
     // Otherwise, paint background:
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + (t) * color(0.5, 0.7, 1.0);
 }
 
@@ -49,6 +24,10 @@ int main()
     const auto aspect_ratio = 16.0 / 10.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    hittable_list world;
+    world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Set up camera parameters
     auto viewport_height = 2.0;
@@ -68,7 +47,7 @@ int main()
             auto u = double(x) / (image_width - 1);
             auto v = double(y) / (image_height - 1);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
---
+++ src/sphere.hpp
@@ -0,0 +1,46 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.hpp"
+#include "vec3.hpp"
+
+class sphere : public hittable {
+    public:
+        sphere() {}
+        sphere(const point3& cen, double r) : center(cen), radius(r) {}
+
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+    public:
+        point3 center;
+        double radius;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0) {
+        return false;
+    }
+
+    auto sqrt_d = std::sqrt(discriminant);
+    auto root = (-half_b - sqrt_d) / a;
+    if (root < t_min || root > t_max) {
+        root = (-half_b + sqrt_d) / a;
+        if (root < t_min || root > t_max) {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+    return true;
+}
+
+#endif
