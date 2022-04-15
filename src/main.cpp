907c0b39c92285a7c935945c34908d6153824e68
Michael Scholz <m@scholz.moe>
2022-04-15 10:34:44 +0100

    Shade based on surface normals

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "ray.hpp"
 #include "vec3.hpp"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     // We're looking to solve (P(t) - C)(P(t) - C) = r^2
     // This expands to:
     //   b * b * t^2 + 2 * (A - C) * b  * t + (A - C) * (A - C) * r^2 = 0
@@ -22,15 +22,24 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
     auto c = dot(oc, oc) - radius * radius;
     // \Delta = b^2 - 4 * a * c
     auto discriminant = b * b - 4.0 * a * c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    // If the ray hits the sphere:
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
+
+    // Otherwise, paint background:
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + (t) * color(0.5, 0.7, 1.0);
 }
 
