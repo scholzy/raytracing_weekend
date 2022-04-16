fc70a22af6930ba00583c688da2e168a85c14f9a
Michael Scholz <m@scholz.moe>
2022-04-16 09:21:30 +0100

    Implement antialiasing

--- src/color.hpp
+++ src/color.hpp
@@ -1,13 +1,23 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "rtweekend.hpp"
 #include "vec3.hpp"
 
-void write_color(std::ostream &out, color pixel_color) {
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
     // Output the pixel color in range [0, 255]
-    out << static_cast<int>(255.999 * pixel_color.x()) << " "
-        << static_cast<int>(255.999 * pixel_color.y()) << " "
-        << static_cast<int>(255.999 * pixel_color.z()) << std::endl;
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << "\n";
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "rtweekend.hpp"
 
+#include "camera.hpp"
 #include "color.hpp"
 #include "hittable_list.hpp"
 #include "sphere.hpp"
@@ -24,31 +25,27 @@ int main()
     const auto aspect_ratio = 16.0 / 10.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     hittable_list world;
     world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    // Set up camera parameters
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0.0, 0.0);
-    auto vertical = vec3(0.0, viewport_height, 0.0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    camera cam;
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int y = image_height - 1; y >= 0; --y) {
         std::cerr << "\rScanlines remaining: " << y << " " << std::flush;
         for (int x = 0; x < image_width; ++x) {
-            auto u = double(x) / (image_width - 1);
-            auto v = double(y) / (image_height - 1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (double(x) + random_double()) / (image_width - 1);
+                auto v = (double(y) + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+        write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
